38f46f6114141856e0a84f1412d4ae7794fb265e
Jpta24 <jeanpierretorres@gmail.com>
2022-09-12 15:32:16 +0200

    auth routes and views without style

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, /* -> Ideally, should be unique, but its up to you */
+      require:true
+    },
+    avatarUrl: String,
+    googleId: String,
+    googleAvatarUrl: String,
+    password: String,
+    email: {
+      type: String,
+      unique: true
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+    },
+    playlists: [{
+      type: Schema.Types.ObjectId,
+		  // this refers to the model the id above belongs to
+		  ref: 'Playlist'
+    }],
+    videoNotes:[{
+      type: Schema.Types.ObjectId,
+		  // this refers to the model the id above belongs to
+		  ref: 'Video',
+      notes:[{
+        time: String,
+        note:String,
+        timestamps: true
+      }]
+    }]
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    versionKey: false, //   versionKey: false,  remove version _V
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;

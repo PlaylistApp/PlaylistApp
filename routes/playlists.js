fcf92172759e1b9ca87bd2c2b3e32d9af47d0c4b
Jpta24 <jeanpierretorres@gmail.com>
2022-09-14 15:22:46 +0200

    WIP Video view

--- routes/playlists.js
+++ routes/playlists.js
@@ -151,4 +151,14 @@ router.post('/edit-video/:playlistID/:videoID', (req, res, next) => {
 		.catch((err) => next(err));
 });
 
+router.get("/:playlistID/:videoID", (req, res, next) => {
+	Video.findById(req.params.videoID)
+	.populate('playlistID').populate('playlistCreator').populate({})
+		.then((video) => {
+			console.log(video)
+			res.render("videos/view", { video });
+		})
+		.catch((err) => next(err));
+  });
+
 module.exports = router;
